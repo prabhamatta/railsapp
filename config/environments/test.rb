6bed5e228fcfad2e3f13a8a7f0c9be93d3e6c123
Prabha <prabha@prabha.local>
2014-08-21 02:04:34 -0700

    fixed errors in test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,6 @@
-Rails.application.configure do
+# Rails.application.configure do
+Railsapp::Application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
