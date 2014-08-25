2993a914c673197e07656f570b0403a13bfee3e5
Prabha Matta <prabha.matta@gmail.com>
2014-08-24 21:53:45 -0700

    dynamic pages in static html

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,26 +1,25 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
   describe "Home page" do
+    before { visit root_path }
     it "should have the content 'Mini Rails App'" do
-      visit root_path
       expect(page).to have_content('Mini Rails App')
     end
 
     it "should have the base title" do
-      visit root_path
       expect(page).to have_title("Hello World from Prabha")
     end
 
     it "should not have a custom page title" do
-      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
     it "should have the content 'Help'" do
-      visit help_path
       expect(page).to have_content('Help')
     end
 
@@ -31,26 +30,23 @@ describe "Static pages" do
   end
 
  describe "About page" do
+    before { visit about_path }
     it "should have the content 'About Us'" do
-      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the right title" do
-      visit about_path
       expect(page).to have_title("About Us")
     end
   end
   
   describe "Contact page" do
-
+    before { visit contact_path }
     it "should have the content 'Contact'" do
-      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit contact_path
       expect(page).to have_title(" Contact")
     end
   end
